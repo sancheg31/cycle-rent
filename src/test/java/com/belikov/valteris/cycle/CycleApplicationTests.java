6b0ba74083e34211e8c9cdde75317a85b9f0dffc
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-09-02 14:56:51 +0300

    Initial commit

---
+++ src/test/java/com/belikov/valteris/cycle/CycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belikov.valteris.cycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
