69949503b2f2f4957fa83d9f6d6c11b628450bd8
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-09-03 16:05:26 +0300

    Fix typo in controller name'

--- src/main/java/com/belikov/valteris/cycle/bicycle/BicycleController.java
+++ src/main/java/com/belikov/valteris/cycle/bicycle/BicycleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController("/bicycles")
-public class Controller {
+public class BicycleController {
     @Autowired
     private BicycleService bicycleService;
     @GetMapping("/allPage")
