d430e76330c5be2a2bfaa019992ed14bc331a76b
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-09-04 22:55:47 +0300

    Write stub for detail service

--- src/main/java/com/belikov/valteris/cycle/detail/DetailService.java
+++ src/main/java/com/belikov/valteris/cycle/detail/DetailService.java
@@ -1,4 +1,32 @@
 package com.belikov.valteris.cycle.detail;
 
+import com.belikov.valteris.cycle.detail.model.Detail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DetailService {
+
+    @Autowired
+    private DetailRepository detailRepository;
+
+    public void add(Detail newDetail) {
+        detailRepository.save(newDetail);
+    }
+
+    public List<Detail> getAll() {
+        return detailRepository.findAll();
+    }
+
+    public Optional<Detail> getById(Long id) {
+        return detailRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        detailRepository.deleteById(id);
+    }
+
 }
