5953f61da2ed61e2a13ce6a0eebd0508f88b9b0a
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-09-04 23:24:06 +0300

    Write order service stub

--- src/main/java/com/belikov/valteris/cycle/order/OrderService.java
+++ src/main/java/com/belikov/valteris/cycle/order/OrderService.java
@@ -1,4 +1,32 @@
 package com.belikov.valteris.cycle.order;
 
+import com.belikov.valteris.cycle.order.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public void save(Order newOrder) {
+        orderRepository.save(newOrder);
+    }
+
+    public List<Order> getAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        orderRepository.deleteById(id);
+    }
 }
