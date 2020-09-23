2f05f9ef95ffa8ae10545418870ce52debd0d8cb
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-09-23 13:30:21 +0300

    Database updates

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `bicycle_detail`
 --
-
+USE `bicycle_rent`;
 DROP TABLE IF EXISTS `bicycle_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
